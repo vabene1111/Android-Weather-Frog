325f7749d85c090d1c4d363e6daca6fa7b632911
vabene1111 <vabene1234@googlemail.com>
2018-07-09 00:46:42 +0200

    transformed to "tutorial" app

--- app/src/main/java/de/droidenschmiede/weather/MainActivity.java
+++ app/src/main/java/de/droidenschmiede/weather/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_main);
+
+        Button btnWeather = findViewById(R.id.btn_main_openWeather);
+        btnWeather.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openWeatherApp();
+            }
+        });
+
+    }
+
+    private void openWeatherApp(){
         Intent intent = new Intent("android.intent.action.VIEW");
         intent.setData(Uri.parse("dynact://velour/weather/ProxyActivity"));
         intent.setClassName("com.google.android.googlequicksearchbox", "com.google.android.apps.gsa.velour.DynamicActivityTrampoline");
